ef9fef50aebf2d2e86c2cf1423b6e1217dc0e4f5
Will Blake <willblakebooking@gmail.com>
2025-03-06 13:13:39 -0600

    Add test cases

--- dist/exercises/makeChange.test.js
+++ dist/exercises/makeChange.test.js
@@ -8,5 +8,10 @@ describe('makeChange', () => {
         expect((0, makeChange_1.makeChange)([5, 10, 25], 15)).toBe(2);
         expect((0, makeChange_1.makeChange)([5, 10, 25], 20)).toBe(2);
         expect((0, makeChange_1.makeChange)([5, 10, 25], 25)).toBe(1);
+        expect((0, makeChange_1.makeChange)([5, 10, 25], 30)).toBe(2);
+        expect((0, makeChange_1.makeChange)([5, 10, 25], 35)).toBe(2);
+        expect((0, makeChange_1.makeChange)([5, 10, 25], 40)).toBe(3);
+        expect((0, makeChange_1.makeChange)([5, 10, 25], 45)).toBe(3);
+        expect((0, makeChange_1.makeChange)([5, 10, 25], 50)).toBe(2);
     });
 });
--- src/exercises/makeChange.test.ts
+++ src/exercises/makeChange.test.ts
@@ -7,5 +7,10 @@ describe('makeChange', () => {
     expect(makeChange([5, 10, 25], 15)).toBe(2);
     expect(makeChange([5, 10, 25], 20)).toBe(2);
     expect(makeChange([5, 10, 25], 25)).toBe(1);
+    expect(makeChange([5, 10, 25], 30)).toBe(2);
+    expect(makeChange([5, 10, 25], 35)).toBe(2);
+    expect(makeChange([5, 10, 25], 40)).toBe(3);
+    expect(makeChange([5, 10, 25], 45)).toBe(3);
+    expect(makeChange([5, 10, 25], 50)).toBe(2);
   });
 });
